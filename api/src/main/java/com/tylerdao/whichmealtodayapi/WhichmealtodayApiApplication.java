8bbd2f756574997ab5092e7f10dbc43d8b6d5dc7
Thanh <daokythanh.343@gmail.com>
2022-03-29 17:03:00 +0700

    Create api folder

---
+++ api/src/main/java/com/tylerdao/whichmealtodayapi/WhichmealtodayApiApplication.java
@@ -0,0 +1,13 @@
+package com.tylerdao.whichmealtodayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhichmealtodayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhichmealtodayApiApplication.class, args);
+    }
+
+}
